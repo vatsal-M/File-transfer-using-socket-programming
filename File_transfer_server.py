2b940c20ce83f7faa3c830f281fb2d971e57be46
Vatsal Mishra <64276877+vatsal-M@users.noreply.github.com>
2020-05-29 09:00:22 +0530

    Add files via upload

--- File_transfer_server.py
+++ File_transfer_server.py
@@ -1,5 +1,4 @@
 import socket
-import json
 server=socket.socket()
 ip='192.168.0.10'
 port=2000
@@ -8,15 +7,16 @@ server.listen()
 print(f'server made with ip:{ip} and port:{port}')
 client,addr=server.accept()
 print(f'\nClient: {addr[0]}:{addr[1]}')
-path=input('Enter the path of the file you want to send: ')
-file=open(path,"rb")
 fname=input('Enter the name of the file: ').encode()
 client.send(fname)
+path=input('Enter the path of the file you want to send: ')
+file=open(path,"rb")
 while True:
-    fdata=json.load(file)
+    fdata=file.readlines()
     for line in fdata:
         client.send(line)
-    client.send(json.load(1))
+    eof='1'.encode()
+    client.send(eof)
     print('file has been received\nServer is closing')
     client.close()
     break
