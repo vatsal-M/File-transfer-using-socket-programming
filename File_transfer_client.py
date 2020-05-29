afd721c114e037ffad709ece52bbd2fd66b6aee0
Vatsal Mishra <64276877+vatsal-M@users.noreply.github.com>
2020-05-29 09:00:36 +0530

    Add files via upload

--- File_transfer_client.py
+++ File_transfer_client.py
@@ -1,18 +1,17 @@
 import socket
-import json
 client=socket.socket()
 client.connect(('192.168.0.10',2000))
 print('Client is connected to server ip:192.168.0.10\n\n')
 fname=client.recv(1024).decode()
-file=open(f'Downloads\{fname}','wb')
+print(f'file is {fname}')
+file=open(r'C:\Users\Vatsal pc\Downloads\{name}'.format(name=fname),'wb')
 while True:
-    line='oo'
-    while line!=1:
-        line=client.recv(1024)
-        json.dump(line,file)
-    print('file has been recieved in the Downloads folder')
-    client.close()
-    break
+    line=client.recv(1024)
+    file.write(line)
+    if line=='1':
+        print('file has been recieved in the Downloadsfolder')
+        client.close()
+        break
 file.close()
 
     
